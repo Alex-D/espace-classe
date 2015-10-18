c4371e582146fc39fffa1821ab2a1469c62c3a82
Alex-D <demodealex@gmail.com>
2015-10-18 02:03:27 +0200

    feat(classroom): add participation and warning edit + improve note button and edit form

--- src/classroom/controller/ClassroomController.js
+++ src/classroom/controller/ClassroomController.js
@@ -9,15 +9,17 @@ angular.module('espaceClasse.classroom')
         $scope.classroom = [];
         let seats = [];
         let student = {
-            firstname: "Jean-Michel",
-            lastname: "Deschamps"
+            firstname: 'Jean-Michel',
+            lastname: 'Deschamps',
+            participationEditIsVisible: false,
+            warningEditIsVisible: false
         };
         for (let i = 0; i < 10; i += 1) {
             let newStudent = angular.copy(student);
             newStudent.participations = i * 2;
             newStudent.photo = 'https://randomuser.me/api/portraits/med/' + (i % 2 === 0 ? 'men' : 'women') + '/' + i + '.jpg';
             if (i % 4 < 2) {
-                newStudent.note = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, temporibus autem. Saepe eligendi magni, voluptate quas consectetur nesciunt! Tempore cupiditate labore, odio laudantium ea. Aspernatur expedita eaque voluptate perspiciatis commodi!";
+                newStudent.note = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, temporibus autem. Saepe eligendi magni, voluptate quas consectetur nesciunt! Tempore cupiditate labore, odio laudantium ea. Aspernatur expedita eaque voluptate perspiciatis commodi!';
             }
             newStudent.warning = Math.round(i / 2);
             seats.push(i !== 1 && i !== 4 && i !== 7 ? newStudent : {});
@@ -35,7 +37,21 @@ angular.module('espaceClasse.classroom')
             $scope.studentNotePopin = null;
         };
 
-        // TODO: Modify student participations
+        // Modify student participations
+        $scope.toggleEditParticipations = function (student) {
+            student.participationEditIsVisible = !student.participationEditIsVisible;
+        };
+        $scope.editParticipations = function (student, delta) {
+            student.participations += delta;
+            student.participationEditIsVisible = false;
+        };
 
-        // TODO: Modify warnings
+        // Modify warnings
+        $scope.toggleEditWarning = function (student) {
+            student.warningEditIsVisible = !student.warningEditIsVisible;
+        };
+        $scope.setWarning = function (student, warningLevel) {
+            student.warning = warningLevel;
+            student.warningEditIsVisible = false;
+        };
     });
